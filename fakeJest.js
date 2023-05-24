5d0865ccd694f3b4d791cd809b6ae43025efbb18
anderson.morais <anderson.morais@hemoce.ce.gov.brgit config --global user.email anderson.morais@hemoce.ce.gov.br>
2023-05-23 21:17:31 -0300

    criando mais expect

--- fakeJest.js
+++ fakeJest.js
@@ -12,6 +12,48 @@ const expect = (result)=>({
     }else{
         console.log('\x1b[31m','🔥 ------------------> Teste Falhou!');  
     }  
+   },
+   toBeTruthy:()=>{
+    if(result){
+        console.log('\x1b[32m','🚀 ------------------> Teste Passou!');
+    }else{
+        console.log('\x1b[31m','🔥 ------------------> Teste Falhou!');  
+    }
+   },
+   toBeFalsy:()=>{
+    if(!result){
+        console.log('\x1b[32m','🚀 ------------------> Teste Passou!');
+    }else{
+        console.log('\x1b[31m','🔥 ------------------> Teste Falhou!');  
+    }
+   },
+   toHaveLength:()=>{
+    if(result.lenght != 0){
+        console.log('\x1b[32m','🚀 ------------------> Teste Passou!');
+    }else{
+        console.log('\x1b[31m','🔥 ------------------> Teste Falhou!');  
+    }
+   },
+   toBeInstanceOf:(expected)=>{
+    if(typeof result == typeof expected){
+        console.log('\x1b[32m','🚀 ------------------> Teste Passou!');
+    }else{
+        console.log('\x1b[31m','🔥 ------------------> Teste Falhou!');  
+    }
+   },
+   toBeNull:()=>{
+    if(result === null){
+        console.log('\x1b[32m','🚀 ------------------> Teste Passou!');
+    }else{
+        console.log('\x1b[31m','🔥 ------------------> Teste Falhou!');  
+    }
+   },
+   toBeNaN:()=>{
+    if(result === 'NaN'){
+        console.log('\x1b[32m','🚀 ------------------> Teste Passou!');
+    }else{
+        console.log('\x1b[31m','🔥 ------------------> Teste Falhou!');  
+    }
    }
 });
 
